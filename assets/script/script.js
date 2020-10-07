f50884a6871fced48d48731951574682257a9d8c
Parker Morgan <jpmaster13@msn.com>
2020-10-07 16:55:38 -0600

    Added the ability to submit an answer with no functionality

--- assets/script/script.js
+++ assets/script/script.js
@@ -9,8 +9,10 @@ var aBox1 = document.getElementById("aBox1");
 var aBox2 = document.getElementById("aBox2");
 var aBox3 = document.getElementById("aBox3");
 var aBox4 = document.getElementById("aBox4");
+var aBoxEl = document.getElementById("choices");
 var timeEl = document.getElementById("hBox2");
 var scoreEl = document.getElementById("hBox3");
+
 
 //timer
 var timeLeft = 5;
@@ -33,6 +35,7 @@ function startTimer(){
 function tick(){
     timeElapsed++;
     console.log(timeElapsed)
+    //end timer
     if(timeElapsed+1>timeLeft){
         clearInterval(timer);
     }
@@ -44,5 +47,16 @@ function updateTimer(){
     timeEl.textContent = currentTime;
 }
 
+//On click to answer boxes, do something
+function submitAnswer(){
+    
+}
+
 //buttons
 startButton.addEventListener("click", startGame);
+//button that gets choice
+aBoxEl.addEventListener('click', function(event){
+    var choice = event.target.getAttribute('id');
+    console.log(choice);
+
+});
