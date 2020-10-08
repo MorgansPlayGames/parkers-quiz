0b349622a5d2eb0bdace611e761ee0106957f7a1
Parker Morgan <jpmaster13@msn.com>
2020-10-07 22:23:48 -0600

    Added filler questions and randomizer functionality. Added arbitrary score and correct answer check

--- assets/script/script.js
+++ assets/script/script.js
@@ -13,7 +13,6 @@ var aBoxEl = document.getElementById("choices");
 var timeEl = document.getElementById("hBox2");
 var scoreEl = document.getElementById("hBox3");
 
-
 //timer
 var timeLeft = 5;
 var timeElapsed;
@@ -22,6 +21,9 @@ var timer;
 //var answer
 var correctAnswer = 1;
 var points = 0;
+var questionIndex = 0;
+
+var questionList = [];
 
 
 //Start function
@@ -29,17 +31,24 @@ function startGame(){
     console.log("Hello!");
     startTimer();
     points = 0;
+    initializeQuestions()
+
 }
 
 //timer Functions
 function startTimer(){
     timer = setInterval(tick, 1000);
     timeElapsed = 0;
+    initializeQuestions();
+    questionIndex = 0;
+    newQuestion();
+    
 }
 
+//every second do this
 function tick(){
     timeElapsed++;
-    console.log(timeElapsed)
+    //console.log(timeElapsed)
     //end timer
     if(timeElapsed+1>timeLeft){
         clearInterval(timer);
@@ -47,6 +56,7 @@ function tick(){
     updateTimer();
 }
 
+//change the time left
 function updateTimer(){
     var currentTime = timeLeft - timeElapsed
     timeEl.textContent = currentTime;
@@ -54,16 +64,83 @@ function updateTimer(){
 
 //On click to answer boxes, do something
 function submitAnswer(choice){
-    console.log(choice);
+    //console.log(choice);
+    //if choice is correct give 5 points
     if(choice === correctAnswer){
         console.log("correct");
         points = points + 5;
     }else{
+        //do something on incorrect
         console.log("incorrect");
     }
     console.log("points " + points);
     scoreEl.textContent = "Score: " + points;
+    newQuestion();
 }
+
+//array of questions to put into questionList
+function initializeQuestions(){
+    questionList[0] = ["What is my name?", "Parker", "Joel", "Matthew", "Jimmothy"];
+    questionList[1] = ["A string is a:", "Series of characters", "Function that goes to another function", "A cat toy", "good album"];
+    questionList[2] = ["The answer is 'C'", "C", "A", "B", "D"];
+    shuffle(questionList);
+}
+
+function newQuestion(){
+    //if there are questions left
+    if(questionIndex < questionList.length){
+    qBox.textContent = questionList[questionIndex][0];
+    
+    //Shuffle and populate answers assign correct answer to global
+    var numbs = [1, 2, 3, 4]
+    shuffle(numbs);
+    var a = numbs[0];
+    if(a === 1){correctAnswer = 1}
+    var b = numbs[1];
+    if(b === 1){correctAnswer = 2}
+    var c = numbs[2];
+    if(c === 1){correctAnswer = 3}
+    var d = numbs[3];
+    if(d === 1){correctAnswer = 4}
+    aBox1.textContent = questionList[questionIndex][a];
+    aBox2.textContent = questionList[questionIndex][b];
+    aBox3.textContent = questionList[questionIndex][c];
+    aBox4.textContent = questionList[questionIndex][d];
+        console.log(questionIndex);
+    questionIndex++;
+
+    }else{
+        //when questions are out
+        endGame();
+    }
+}
+
+//On game end do this
+function endGame(){
+    console.log("good job");
+}
+
+
+//Shuffle function found to add randomness
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
 
 //buttons
 startButton.addEventListener("click", startGame);
